7a3dea3ac96f5f7719f93f171dd0aa4bfbc222da
alexg622 <alex.gm62288@gmail.com>
2017-03-09 10:10:52 -0800

    start user auth make routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :login, :logout
+
+  def login(user)
+  	session[:user_id] = user.id
+  end 
+
+  def logout
+  	session[:user_id] = nil
+  end
+
+  def current_user
+  	if session[:user_id]
+  		User.find(session[:user_id])
+  	else 
+  		nil
+  	end 
+  end   
 end
