d21ef9cb847898ec4be234fb2ef81737d2bc5bb0
alexg622 <alex.gm62288@gmail.com>
2017-03-09 11:59:28 -0800

    Make user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :sessions 
+	root "categories#index"
+  resources :sessions
   resources :users
+
+  get '/logout' => 'sessions#destroy'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
