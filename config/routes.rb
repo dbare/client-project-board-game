4156e40a88fd19d40fd31f5eebf963d771f8689d
Davison Bare <davis@Davisons-MacBook-Pro.local>
2017-03-09 10:44:26 -0800

    conflict resolved

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   resources :sessions 
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
