d6c20421c383428ee0c0a58f12a7f25a631e1f41
Davison Bare <davis@Davisons-MacBook-Pro.local>
2017-03-10 15:32:41 -0800

    adds more games and cagtegories  to the site

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,24 @@ Category.create(name: "Strategy")
 Category.create(name: "Kids")
 Category.create(name: "Party")
 Category.create(name: "Fantasy")
+Category.create(name: "Card Games")
 
 # Games
-a = Game.create(title: "Settlers", description: "a way to ruin friendships", category_id: 2)
-b = Game.create(title: "Monopoly", description: "another way to ruin friendships", category_id: 2)
-c = Game.create(title: "Connect Four", description: "line up four of your pieces to win", category_id: 3)
-d = Game.create(title: "Ride the Bus", description: "fun drinking game", category_id: 4)
+a = Game.create(title: "Settlers", description: "A way to ruin friendships", category_id: 2)
+b = Game.create(title: "Monopoly", description: "Another way to ruin friendships", category_id: 2)
+c = Game.create(title: "Connect Four", description: "Line up four of your pieces to win", category_id: 3)
+d = Game.create(title: "Ride the Bus", description: "Fun drinking game", category_id: 4)
 e = Game.create(title: "D&D", description: "classic table top game", category_id: 1)
+f = Game.create(title: "Chess", description: "Kill the enemy King", category_id: 2)
 g = Game.create(title: "Game of Thrones", description: "When you play the game of war, you win or you die", category_id: 5)
+h = Game.create(title: "Risk", description: "Take over the world", category_id: 2)
+i = Game.create(title: "Uno", description: "Get rid of all the cards in your hand", category_id: 6)
+j = Game.create(title: "Cards Against Humanity", description: "Learn which of your friends are jerks", category_id: 6)
+k = Game.create(title: "Candy Land", description: "Travel through a world of candy", category_id: 3)
+l = Game.create(title: "Apples to Apples", description: "Cards Against Humanity Lite", category_id: 3)
+m = Game.create(title: "Kings Cup", description: "Drink your way through a deck of cards", category_id: 4)
+n = Game.create(title: "Arkham Horror", description: "Weird game", category_id: 1)
+o = Game.create(title: "Lord of the Rings", description: "A guy named Dumbledore has to return The Ring to forest moon of Endor", category_id: 5)
 
 # Votes
 a.votes.create(value: 2, user_id: 1)
@@ -29,7 +39,6 @@ g.votes.create(value: 1, user_id: 2)
 
 # Comments
 Comment.create(body: "this game is fun", user_id: 1, game_id: 1)
-Comment.create(body: "bad game", user_id: 1, game_id: 1)
 Comment.create(body: "I'm bad at this game", user_id: 3, game_id: 1)
 
 Comment.create(body: "good game", user_id: 2, game_id: 2)
